555477619791c3056136dbc9b8aba3a43843af2a
nju_zxd_531 <1149676299@qq.com>
2021-06-16 21:45:07 +0800

    Project structure establishment

--- service/service_hosp/src/main/java/com/nju/hregister/hosp/config/HospConfig.java
+++ service/service_hosp/src/main/java/com/nju/hregister/hosp/config/HospConfig.java
@@ -1,10 +1,20 @@
 package com.nju.hregister.hosp.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @MapperScan("com.nju.hregister.hosp.mapper")
 public class HospConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
 }
